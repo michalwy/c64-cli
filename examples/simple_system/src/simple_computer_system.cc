0f75c32777c5296ceda8bafcd94c1e1f92abea9e
Michał Wysoczański <michalwy@gmail.com>
2015-06-07 18:54:39 +0200

    Remove simple_system example

--- examples/simple_system/src/simple_computer_system.cc
+++
@@ -1,9 +0,0 @@
-#include "simple_computer_system.hh"
-
-simple_computer_system::simple_computer_system()
-	: harpoon::computer_system("Simple computer system") {
-
-}
-
-simple_computer_system::~simple_computer_system() {}
-
