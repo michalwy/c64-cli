ae453264e0a8c27966a573e84d61af9b5de5b37a
Michał Wysoczański <michalwy@gmail.com>
2019-07-11 23:03:44 +0200

    address_range: Add intersection utility methods

--- harpoon/include/harpoon/memory/address_range.hh
+++ harpoon/include/harpoon/memory/address_range.hh
@@ -11,15 +11,27 @@
 namespace harpoon {
 namespace memory {
 
+/*
+ * Internally _end is the address of next byte after memory range
+ * Valid addresses in range are [_start, _end-1]
+ */
 class address_range {
 public:
 	address_range() {}
-	address_range(const address &start, const address &end) : _start(start), _end(end) {}
+	address_range(const address &start, const address &end) : _start(start), _end(end + 1) {}
 	address_range(const address_range &) = default;
 	address_range &operator=(const address_range &) = default;
 
 	bool operator==(const address_range &second) const {
 		return get_start() == second.get_start() && get_end() == second.get_end();
+	}
+
+	operator bool() const {
+		return !is_empty();
+	}
+
+	bool is_empty() const {
+		return _start == _end;
 	}
 
 	address get_start() const {
@@ -31,19 +43,19 @@ public:
 	}
 
 	address get_end() const {
-		return _end;
+		return _end - 1;
 	}
 
 	void set_end(address end) {
-		_end = end;
+		_end = end + 1;
 	}
 
 	std::uint_fast64_t get_length() const {
-		return _end - _start + 1;
+		return _end - _start;
 	}
 
 	void set_length(std::uint_fast64_t length) {
-		_end = _start + length - 1;
+		_end = _start + length;
 	}
 
 	void set_range(address start, address end) {
@@ -68,6 +80,22 @@ public:
 		return has_address(second.get_start()) || has_address(second.get_end());
 	}
 
+	address_range get_intersection(const address_range &r) const {
+		address_range n = r;
+		n.intersect(*this);
+		return n;
+	}
+
+	void intersect(const address_range &r) {
+		address s = std::max(_start, r._start);
+		address e = std::min(_end, r._end);
+		if (e < s) {
+			e = s = 0;
+		}
+		_start = s;
+		_end = e;
+	}
+
 	static inline address max() {
 		return std::numeric_limits<address>::max();
 	}
@@ -75,11 +103,15 @@ public:
 	friend std::ostream &operator<<(std::ostream &stream, const address_range &range) {
 		std::ostream out(stream.rdbuf());
 		out << "[";
-		out << std::hex << std::setw(sizeof(address) * 2) << std::uppercase << std::setfill('0')
-		    << range._start;
-		out << ", ";
-		out << std::hex << std::setw(sizeof(address) * 2) << std::uppercase << std::setfill('0')
-		    << range._end;
+		if (range.is_empty()) {
+			out << "empty";
+		} else {
+			out << std::hex << std::setw(sizeof(address) * 2) << std::uppercase << std::setfill('0')
+			    << range._start;
+			out << ", ";
+			out << std::hex << std::setw(sizeof(address) * 2) << std::uppercase << std::setfill('0')
+			    << range._end - 1;
+		}
 		out << "]";
 		return stream;
 	}
